79b2092b5866b41727fc9244ab115d3790a3020c
Peter Noyes <pnoyes@gmail.com>
2016-04-13 21:02:32 -0700

    Now demonstrates boolean drawing operations

--- main.c
+++ main.c
@@ -2,21 +2,53 @@
 #include <stdlib.h>
 #include "api.h"
 
+static unsigned char const _ball[8] = { 0x3c, 0x42, 0x81, 0x81, 0x81, 0x81, 0x42, 0x3c }; 
+static unsigned char const _ball_mask[8] = { 0xc3, 0x81, 0x00, 0x00, 0x00, 0x00, 0x81, 0xc3 };
+static unsigned char const _hatch[8] = { 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa };
+
 int main() {
-	unsigned char x = 0;
+	unsigned char* ball = (unsigned char*)_ball;
+	unsigned char* ball_mask = (unsigned char*)_ball_mask;
+	unsigned char* hatch = (unsigned char*)_hatch;
+
+	unsigned char p, x, y, dir = 0;
 	
+	x = 10;
+
 	api_init();					// Initialize the API
 
 	CLEAR();					// Clear the screen
 
 
 	for (;;) {
-		SET_PIXEL(x, 10, 1);	// Set pixel
+		for (p = 0; p < 128; p += 8) {
+			DRAW_SPRITE(hatch, p, 0, 8, 8, 0, DRAW_NOP);
+			DRAW_SPRITE(hatch, p, 8, 8, 8, 0, DRAW_NOP);
+		}
+
+		DRAW_SPRITE(ball_mask, x, y, 8, 8, 0, DRAW_AND);
+		DRAW_SPRITE(ball, x, y, 8, 8, 0, DRAW_OR);
 
 		DISPLAY();				// Push contents of video memory to screen (expensive call)
 
-		SET_PIXEL(x, 10, 0);	// Clear Pixel
 		++x;
+		if (x > 120) {
+			x = 0;
+		}
+
+		if (dir == 0) {
+			++y;
+			if (y > 8) {
+				y = 8;
+				dir = 1;
+			}
+		} else if (dir == 1) {
+			--y;
+			if (y > 127) {
+				y = 0;
+				dir = 0;
+			}
+		}
 		
 		WAIT();					// Wait for next interrupt, sync game loop
 	}
